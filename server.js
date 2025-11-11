1944b38b1289723ab69c729cd7cc831241da3750
unknown <hogazme@gmail.com>
2025-11-11 13:19:27 -0600

    updated server.js

--- server.js
+++ server.js
@@ -828,7 +828,7 @@ app.post('/simulation-control', (req, res) => {
 
 // Start the server
 // const PORT = process.env.PORT || 3000;
-httpServer.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
 });
 
